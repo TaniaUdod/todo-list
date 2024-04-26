6b5354b447b20eea42e6076ce6289df442e08683
TaniaUdod <taniaudod28@gmail.com>
2024-04-26 21:28:07 +0300

    deployment to GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/todo-list">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
